70ee4b02736a0383f426a9813ea1bfee685b0cee
Fran Notebook <franciscobonfanti01@gmail.com>
2024-09-17 09:50:51 -0300

    agregado del navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NabVarComponent } from './shared/nab-var/nab-var.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NabVarComponent,
     
   ],
   imports: [
